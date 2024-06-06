fab8a5b728c20d6e24fc00324718b26da7307f74
Guillaume Clabaut <123895070+GClabaut@users.noreply.github.com>
2024-06-06 09:19:59 +0200

    Modified the push function for multiple books, added second book

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ function Book(title, author, page, reading) {
   this.reading = reading;
 }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book)
+function addBookToLibrary(...book) {
+  myLibrary.push(...book)
 }
 
 const book1 = new Book('Flowers for Algernon', 'Daniel Keyes', '311', 'true');
-addBookToLibrary(book1);
+const book2 = new Book('The Silmarillion', 'John Ronald Reuel Tolkien', '365', 'true');
+addBookToLibrary(book1, book2);
 console.log(myLibrary);
+
